92a25c66f724935940ef29b62545fce8cb87c2e5
LeDucViet <ldviettb04@gmail.com>
2025-05-13 00:27:14 +0700

    add variable data type

---
+++ Bai1/thuchanh/src/VariableDataType.java
@@ -0,0 +1,17 @@
+public class VariableDataType {
+    public static void main(String[] args) {
+        int i = 10;
+        float f = 20.5f;
+        double d = 30.5;
+        char c = 'A';
+        boolean b = true;
+        String s = "Hello";
+
+        System.out.println("i = " + i);
+        System.out.println("f = " + f);
+        System.out.println("d = " + d);
+        System.out.println("c = " + c);
+        System.out.println("b = " + b);
+        System.out.println("s = " + s);
+    }
+}
---
+++ Bai2/thuchanh/src/dao_nguoc_so_nguyen.java
@@ -0,0 +1,4 @@
+package PACKAGE_NAME;
+
+public class dao_nguoc_so_nguyen {
+}
