26a6e193381e46d673ec5814ebebf8c596c3bb81
LeDucViet <ldviettb04@gmail.com>
2025-05-14 14:13:49 +0700

    add interest

--- Bai1/thuchanh/src/Interest.java
+++ Bai1/thuchanh/src/Interest.java
@@ -1,4 +1,21 @@
-package PACKAGE_NAME;
+import java.util.Scanner;
 
 public class Interest {
+    public static void main(String[] args) {
+        double money = 1.0;
+        int month = 1;
+        double interestRate = 1.0;
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter investment amount: ");
+        money = input.nextDouble();
+        System.out.print("Enter number of months: ");
+        month = input.nextInt();
+        System.out.print("Enter annual interest rate in percentage: ");
+        interestRate = input.nextDouble();
+        double totalInterest = 0;
+        for (int i=0; i<month; i++){
+            totalInterest += money * (interestRate / 100)/12 * month;
+        }
+        System.out.println("Total interest earned: " + totalInterest);
+    }
 }
